7c9cc3dc02b5a9e6ef6b13d602beb8821cefa008
karolyiar <karolyiar@gmail.com>
2019-06-09 14:27:10 +0200

    moved passwords file

---
+++ passwords_fillout.h
@@ -0,0 +1,10 @@
+#ifndef PASSWORDS_H
+#define PASSWORDS_H
+
+#define WIFI_SSID "xxx"
+#define WIFI_PASSWORD "xxxxxxxx"
+#define MQTT_SERVER "192.168.xxx.xxx"
+#define MQTT_USER "xxx"
+#define MQTT_PASSWORD "xxxxxxxx"
+
+#endif
